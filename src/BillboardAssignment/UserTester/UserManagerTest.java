f7de530c745a92a501f6eb3313b9a50122b6c475
Sebastian Muir-Smith <sebastian@muirsmith.com>
2020-04-22 16:29:40 +1000

    Add first test and setup for login

--- src/BillboardAssignment/UserTester/UserManagerTest.java
+++ src/BillboardAssignment/UserTester/UserManagerTest.java
@@ -1,17 +1,61 @@
 package BillboardAssignment.UserTester;
 
+import BillboardAssignment.Authentication.*;
+import BillboardAssignment.Database.*;
+import BillboardAssignment.User.UserManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserManagerTest {
 
+    PasswordManager passwords;
+
+    SessionKeyManager keyManager;
+
+    UserAuthDataInput userCreds;
+
+    UserAuthDataOutput userCredsHashed;
+
+    UserManager userManager;
+
     @BeforeEach
-    void setUp() {
+    void setUp() throws DatabaseNotAccessibleException, DatabaseLogicException, DatabaseObjectNotFoundException {
+        Queryable<UserAuthDataOutput> database = new DatabaseArray<UserAuthDataOutput>();
+        database.initialiseDatabase();
+        passwords = new PasswordManager(database);
+
+        Queryable<SessionKeyDataOutput> database2 = new DatabaseArray<SessionKeyDataOutput>();
+        database.initialiseDatabase();
+        keyManager = new SessionKeyManager(database2);
+
+        userCreds = new UserAuthDataInput(1, "123".getBytes());
+
+        passwords.addPasswordData(userCreds);
+
+        userCredsHashed = passwords.passwordDatabase.getObject(1);
+
+        userManager = new UserManager(passwords, keyManager);
     }
 
     @AfterEach
     void tearDown() {
     }
+
+    @Test
+    void login() throws IncorrectPasswordException{
+
+        String sessionKey = userManager.login(userCreds);
+
+        assertTrue(sessionKey instanceof String);
+
+        UserAuthDataInput badCreds = new UserAuthDataInput(1, "the_wrong_password".getBytes());
+
+        assertThrows(IncorrectPasswordException.class, ()->{userManager.login(badCreds);});
+
+
+    }
+
 }
