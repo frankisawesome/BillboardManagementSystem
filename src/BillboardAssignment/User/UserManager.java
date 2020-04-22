d58a89c57b70a552c6e1d16d5cd4a7cd0333b40c
Sebastian Muir-Smith <sebastian@muirsmith.com>
2020-04-22 16:02:31 +1000

    Add stubs for user and user testing classes

---
+++ src/BillboardAssignment/User/UserManager.java
@@ -0,0 +1,4 @@
+package BillboardAssignment.User;
+
+public class UserManager {
+}
---
+++ src/BillboardAssignment/UserTester/UserManagerTest.java
@@ -0,0 +1,17 @@
+package BillboardAssignment.UserTester;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserManagerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
