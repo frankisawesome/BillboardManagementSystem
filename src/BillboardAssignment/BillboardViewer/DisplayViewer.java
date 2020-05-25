476c09d5313ca9caf0f596cd21844fa192a083cb
Conor O'Reilly <Conor@Conors-MacBook-Pro.local>
2020-05-26 00:40:44 +1000

    Added a 15 second timer in which to update the billboard

--- src/BillboardAssignment/BillboardViewer/DisplayViewer.java
+++ src/BillboardAssignment/BillboardViewer/DisplayViewer.java
@@ -6,32 +6,41 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class DisplayViewer extends JFrame {
 
     public DisplayViewer() throws HeadlessException {
-        setUndecorated(true);
+        setUndecorated(false);
         setResizable(false);
+        setVisible(true);
+
+        addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_ESCAPE)
+                    dispose();
+            }
+        });
+
+        addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                dispose();
+            }
+        });
     }
 
     public static void main(String[] args) {
-        GraphicsEnvironment graphics =
-                GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice device = graphics.getDefaultScreenDevice();
         JFrame Billboard = new DisplayViewer();
+        GraphicsDevice device  = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
         device.setFullScreenWindow(Billboard);
 
-        Billboard.addKeyListener(new KeyAdapter() {
-            public void keyPressed(KeyEvent e) {
-                if(e.getKeyCode() == KeyEvent.VK_ESCAPE)
-                    Billboard.dispose();
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                // Update billboard content
             }
-        });
-
-        Billboard.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e) {
-                Billboard.dispose();
-            }
-        });
+        }, 15000, 15000);
     }
 }
